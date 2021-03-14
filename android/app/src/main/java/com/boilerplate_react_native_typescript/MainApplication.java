07a4f2b0005b554635a5a7056ff580b89e8dfa7a
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-03-14 16:45:35 +0900

    Edit Android build setting for reanimated package

--- android/app/src/main/java/com/boilerplate_react_native_typescript/MainApplication.java
+++ android/app/src/main/java/com/boilerplate_react_native_typescript/MainApplication.java
@@ -1,5 +1,8 @@
 package com.boilerplate_react_native_typescript;
 
+import com.facebook.react.bridge.JSIModulePackage; // <- add
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -32,6 +35,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+            return new ReanimatedJSIModulePackage(); // <- add
         }
       };
 
